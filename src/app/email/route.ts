ff23be72b6c2e5b630fac173e101cb9bf3af1180
PranavK226 <69270517+PranavK226@users.noreply.github.com>
2023-08-17 16:19:44 +0530

    testing formdata requests

--- src/app/email/route.ts
+++ src/app/email/route.ts
@@ -5,14 +5,15 @@ export function GET() {
     return NextResponse.json({ result: "Hi world" });
 }
 
-export async function POST() {
+export async function POST(request: Request) {
+    const req = await request.formData()
+
     sendgrid.setApiKey("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");
     const msg = {
-        to: "pranav.kannepalli@gmail.com", // Change to your recipient
+        to: `${req.get('email')}`, // Change to your recipient
         from: "streeshaktiprabodhan@gmail.com", // Change to your verified sender
-        subject: "Sending with SendGrid is Fun",
-        text: "and easy to do anywhere, even with Node.js",
-        html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+        subject: "Stree Shakti Prabodhini Order",
+        html: `${req.get('body')}`,
     };
     const s = await sendgrid.send(msg)
         .then(() => {
