a194380fae82e312732cb9c5f70208a07c7270a1
PranavK226 <69270517+PranavK226@users.noreply.github.com>
2023-08-17 16:32:26 +0530

    developing api

--- src/app/email/route.ts
+++ src/app/email/route.ts
@@ -13,7 +13,7 @@ export async function POST(request: Request) {
         to: `${req.get('email')}`, // Change to your recipient
         from: "streeshaktiprabodhan@gmail.com", // Change to your verified sender
         subject: "Stree Shakti Prabodhini Order",
-        html: `${req.get('body')}`,
+        html: `<div><p>Hello ${req.get('name')},<p><br/><p>Thank you for your order! Enclosed are your order details:</p><br/>${req.get('items')}<br/><p>Thank you!</p></div>`,
     };
     const s = await sendgrid.send(msg)
         .then(() => {
